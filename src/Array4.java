77abfbedac362007ea207b7673a5d1b091ff2ed1
Thao Nguyen <thaontt1408@gmail.com>
2022-07-23 16:21:04 -0600

    For a given array of size N, write a code to print the reverse of the array

--- src/Array4.java
+++ src/Array4.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Array4 {
+import java.util.Arrays;
+
+/**
+ * For a given array of size N, write a code to print the reverse of the array
+ */
+public class Array4 {
+    public static void main(String[] args) {
+        int [] arr = {1, 20, 13, 14, 59};
+        int[] array = arr;
+        int temp = 0;
+        for (int i = 0; i < array.length / 2; i++) {
+            temp = array[i];
+            array[i] = array[(array.length - 1) - i];
+            array[(array.length-1) - i] = temp;
+        }
+        System.out.println(Arrays.toString(array));
+    }
 }
